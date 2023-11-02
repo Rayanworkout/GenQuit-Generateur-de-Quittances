8c0b8df7bac3e278d4cb16092375cc8ceeb9ad26
Rayan <rayan13170@protonmail.com>
2023-11-02 14:28:56 +0100

    added mirroring

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,21 @@
 
-
 // Fonction to get date
+// id="date-today
+// id = current-month
+
+
+// Get all input fields + preview fields of the page and add an
+// event listener to mirror the content
+
+const inputFields = document.querySelectorAll('input, input[id$="charges-input"], input[id$="loyer-ht-input"]');
+const previewElements = document.querySelectorAll('p[id$="-preview"], span[id$="-preview"], [id$="charges"], [id$="loyer-ht"]');
+
+
+inputFields.forEach((input, index) => {
+    input.addEventListener('input', function () {
+        const inputValue = input.value;
+        previewElements[index].textContent = inputValue;
+    });
+
+});
+
