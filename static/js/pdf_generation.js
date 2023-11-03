d54c7a7df5736a487dae10bf1506111d00723d3b
Rayan <rayan13170@protonmail.com>
2023-11-03 16:08:13 +0100

    updated pdf function

--- static/js/pdf_generation.js
+++ static/js/pdf_generation.js
@@ -1,67 +1,75 @@
 import { jsPDF } from "jspdf";
 
-const doc = new jsPDF({
-  orientation: 'portrait',
-  unit: 'mm',
-  format: 'a4',
-});
 
-doc.setFontSize(12);
+function createQuittance(proprioName,
+  proprioAddress,
+  locataireName,
+  locataireAddress,
+  month,
+  date,
+  periodeFrom,
+  periodeTo,
+  loyerHc,
+  charges,
+  total) {
 
-doc.text('{Jane Smith}', 10, 13);
+  const doc = new jsPDF({
+    orientation: 'portrait',
+    unit: 'mm',
+    format: 'a4',
+  });
 
-doc.text('{456 Rue du Propriétaire, 13002 Marseille}', 10, 20);
+  doc.setFontSize(12);
 
+  doc.text(proprioName, 10, 13);
 
-doc.text('John Doe', 130, 13);
-doc.text('{Adresse du locataire: 123 Rue de la Location}', 130, 20);
-
-doc.line(10, 30, 100, 30);
-
-doc.setFontSize(22);
-doc.text('Quittance de {Novembre 2023}', 105, 80, { align: 'center' });
-
-
-// const currentDate = new Date();
-// doc.text('Date du jour: ' + currentDate.toLocaleDateString(), 20, 80);
-
-
-doc.setFontSize(14);
-doc.text('Le {3/11/2023}', 105, 90, { align: 'center' });
-
-doc.setFontSize(16);
-doc.text('Pour la période du {01/11/2023} au {02/12/2023}', 10, 120)
+  // RETOUR À LA LIGNE !
+  doc.text(proprioAddress, 10, 20);
 
 
-doc.text('Calcul', 180, 200)
+  doc.text(locataireName, 130, 13);
 
-doc.line(10, 205, 200, 205);
+  // IDEM
+  doc.text(locataireAddress, 130, 20);
 
-doc.text('Loyer Hors Charges', 10, 215)
+  doc.line(10, 30, 100, 30);
 
-doc.line(10, 220, 200, 220);
-
-doc.text('Charges', 10, 230)
-
-doc.line(10, 235, 200, 235);
-
-doc.text('Total', 10, 245)
+  doc.setFontSize(22);
+  doc.text(`Quittance de ${month}`, 105, 80, { align: 'center' });
 
 
-// // Ajoutez le montant des charges et le total
-// const montantCharges = 200;
+  doc.setFontSize(14);
+  doc.text(`Le ${date}`, 105, 90, { align: 'center' });
 
-// doc.text('Montant des charges: ' + montantCharges + ' €', 20, 90);
-// const montantLoyer = 1000;
-// const total = montantLoyer + montantCharges;
-// doc.text('Total à payer: ' + total + ' €', 20, 100);
-
-// doc.text('Signature du propriétaire: _______________', 105, 260, { align: 'center' });
+  doc.setFontSize(16);
+  doc.text(`Pour la période du ${periodeFrom} au ${periodeTo}`, 10, 120);
 
 
-// Enregistrez le document dans un fichier PDF
-const outputFilename = 'quittance_de_loyer.pdf';
-doc.save(outputFilename);
+  doc.text('Calcul', 180, 200);
 
-console.log(`La quittance de loyer a été générée dans ${outputFilename}`);
-// Made with GenQuit
+  doc.line(10, 205, 200, 205);
+
+  doc.text('Loyer Hors Charges', 10, 215);
+  doc.text(loyerHc, 150, 215)
+
+  doc.line(10, 220, 200, 220);
+
+  doc.text('Charges', 10, 230);
+  doc.text(charges, 150, 230);
+
+  doc.line(10, 235, 200, 235);
+
+  doc.text('Total', 10, 245);
+  doc.text(total, 150, 245);
+
+  doc.text('Signature du propriétaire: _______________', 10, 280);
+
+  doc.setFontSize(9)
+  doc.text('Made with GenQuit', 180, 295);
+
+
+  const outputFilename = 'quittance_de_loyer.pdf';
+  doc.save(outputFilename);
+
+  console.log(`La quittance de loyer a été générée dans ${outputFilename}`);
+}
